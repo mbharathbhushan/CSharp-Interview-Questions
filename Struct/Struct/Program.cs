e69bbeed224ebaab2587a45a6b15d14def71ae82
M.Bharath Bhushan <mbharath278@hotmail.com>
2018-12-07 16:06:54 +0530

    Strcut's in C#
    Strcut's in C#

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee structEmployee = new Employee
+            {
+                id = 100
+            };
+
+            Console.WriteLine(structEmployee.id);
+
+            Console.WriteLine(Update(structEmployee).id);
+            Console.WriteLine(structEmployee.id);
+
+            Console.ReadKey();
+        }
+
+        static Employee Update(Employee emp)
+        {
+            emp.id = 0;
+            return emp;
+        }
+    }
+
+    struct Employee
+    {
+        public int id { get; set; }
+    }
+
+}
